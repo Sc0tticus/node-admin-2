51263aca4db640e5ee83b1e9f70cdb4604b32f5d
Sc0tticus <ssinger303@gmail.com>
2022-03-03 11:23:11 -0700

    CH4 6S. Added Express. Created 1st route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello 2');
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+	origin: ["http://localhost:3000"]
+}));
+
+app.get('/', (req: Request, res: Response) => {
+	res.send("Hello World");
+});
+
+app.listen(8000, () => {
+	console.log('listening to port 8000');
+});
